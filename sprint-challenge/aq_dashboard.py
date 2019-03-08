9deafa36d53c75c6ce4b7aec704b4d77ade24a89
Kailey Smith <krsmith310@gmail.com>
2019-03-08 11:31:49 -0700

    Updated SC files

--- sprint-challenge/aq_dashboard.py
+++ sprint-challenge/aq_dashboard.py
@@ -12,11 +12,30 @@ DB = SQLAlchemy(APP)
 @APP.route('/')
 def root():
     """Base view."""
-    risky = Record.query.filter(Record.value >= 10.0).all()
-    return render_template('base.html', title='Home', records=risky)
+    risky_pm25 = Record.query.filter(Record.value >= 10.0).all()
+    risky_pm10 = RecordPM10.query.filter(RecordPM10.value >= 10.0).all()
+    return render_template('base.html', title='Home', records=risky_pm25,
+                            orecords=risky_pm10)
+
+
+@APP.route('/all')
+def allrecords():
+    """All records view."""
+    allpm25 = Record.query.filter().all()
+    allpm10 = Record.query.filter().all()
+    return render_template('all_rec.html', title='All Records', records=allpm25)
 
 
 class Record(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    datetime = DB.Column(DB.String(25))
+    value = DB.Column(DB.Float, nullable=False)
+
+    def __repr__(self):
+        return '<Time {} --- Value {}>'.format(self.datetime, self.value)
+
+
+class RecordPM10(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     datetime = DB.Column(DB.String(25))
     value = DB.Column(DB.Float, nullable=False)
@@ -34,5 +53,12 @@ def refresh():
     for d in body['results']:
         new_stat = Record(datetime=str(d['date']['utc']), value=d['value'])
         DB.session.add(new_stat)
+    DB.session.commit()
+
+    status, body = api.measurements(city='Los Angeles', parameter='pm10')
+    for d in body['results']:
+        new_stat_pm10 = RecordPM10(datetime=str(d['date']['utc']),
+                                    value=d['value'])
+        DB.session.add(new_stat_pm10)
     DB.session.commit()
     return 'Data refreshed!'
