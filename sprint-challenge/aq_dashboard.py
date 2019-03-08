7d84fe8c674d23e1d1cc069354cbbdf9adfb9bbd
johnpharmd <jhumphre@uw.edu>
2019-03-08 09:43:19 -0800

    Add Part 3 given code to aq_dashboard

--- sprint-challenge/aq_dashboard.py
+++ sprint-challenge/aq_dashboard.py
@@ -1,9 +1,22 @@
 """OpenAQ Air Quality Dashboard with Flask."""
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import openaq
 
 APP = Flask(__name__)
+APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+DB = SQLAlchemy(APP)
 API = openaq.OpenAQ()
+
+
+class Record(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    datetime = DB.Column(DB.String(25))
+    value = DB.Column(DB.Float, nullable=False)
+
+    def __repr__(self):
+        return 'TODO - write a nice representation of Records'
 
 
 @APP.route('/')
@@ -22,3 +35,12 @@ def get_los_angeles_data():
                            dic in body['results']]
 
     return dt_values_tups_list
+
+@APP.route('/refresh')
+def refresh():
+    """Pull fresh data from Open AQ and replace existing data."""
+    DB.drop_all()
+    DB.create_all()
+    # TODO Get data from OpenAQ, make Record objects with it, and add to db
+    DB.session.commit()
+    return 'Data refreshed!'
