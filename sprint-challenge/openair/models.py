ff514cc4d6d146fad4adba13270bfbddc7cfb28d
ed.chin.git <edgardochin@gmail.com>
2019-03-08 12:50:32 -0600

    final commit

---
+++ sprint-challenge/openair/models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+DB = SQLAlchemy()
+
+class Record(DB.Model):
+    """ City data from Openaq API"""
+    id = DB.Column(DB.Integer, primary_key=True)
+    datetime = DB.Column(DB.String(25))
+    value = DB.Column(DB.Float, nullable=False)
+    def __repr__(self):
+        return '<Record {}>'.format(self.name)
