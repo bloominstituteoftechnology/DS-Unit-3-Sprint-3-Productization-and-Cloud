712eeef026d47396cc6923dc2cab40229ebb5bbd
veritaem <dcharris776@gmail.com>
2019-03-08 13:48:11 -0500

    2

--- aq_dashboard.py
+++ aq_dashboard.py
@@ -13,7 +13,7 @@ status, body = api.measurements(city='Los Angeles', parameter='pm25')
 @APP.route('/')
 def root():
     """Base view."""
-    return body['results']
+    return "body['results']['date']['utc'] if i ever figure out how"
 
 class Record(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
@@ -22,24 +22,3 @@ class Record(DB.Model):
 
     def __repr__(self):
         return "Successfully recorded" + '<Record {}>'.format(self.id)
-
-
-@APP.route('/refresh')
-def refresh():
-    """Pull fresh data from Open AQ and replace existing data."""
-    DB.drop_all()
-    DB.create_all()
-    try:
-        db_pull = api.latest(city='Los Angeles', parameter='pm25')
-        DB.session.add(db_pull)
-        for bod in body:
-            db_req = Record()
-            DB.session.add(db_req)
-    except Exception as e:
-        print("Error processing {}: {}".format(username, e))
-        raise e
-    else:
-        DB.session.commit()
-        return 'Data refreshed!'
-
-
