0a996652250aafdd513d655bd94ea0d1f764c965
Zarrina <zarrina_@hotmail.com>
2019-03-10 15:17:23 -0700

    Fetch and render data from OpenQA

--- sprint_challenge/aq_dashboard/aq_dashboard.py
+++ sprint_challenge/aq_dashboard/aq_dashboard.py
@@ -11,4 +11,18 @@ APP = Flask(__name__)
 @APP.route('/')
 def root():
     """Base view."""
-    return 'TODO - part 2 and beyond!'
+    api_data = get_api_data()
+    c = str(api_data)
+    return c
+
+
+def get_api_data():
+
+    """ Query OpenAQ for LA and pm=25"""
+    api = openaq.OpenAQ()
+    data = []
+    status, resp = api.measurements(city='Los Angeles', parameter='pm25')
+    api_data = [(item['date']['utc'], item['value'])
+                    for item in resp['results']]
+
+    return api_data
