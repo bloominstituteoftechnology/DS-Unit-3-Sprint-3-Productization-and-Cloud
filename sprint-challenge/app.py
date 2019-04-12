438ab8dbfdabd1fb0fdfa1af15a6f18dff6ce8c4
Cole Hudson <cole@colejhudson.com>
2019-04-12 09:51:18 -0700

    added part 4

--- sprint-challenge/app.py
+++ sprint-challenge/app.py
@@ -1,5 +1,5 @@
+from flask import Flask, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
 import openaq
 import json
 
@@ -11,11 +11,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
 class Record(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
+    city = db.Column(db.Text, nullable=False)
     datetime = db.Column(db.String(25))
     value = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
-        return '<Record id={} datetime={} value={}>'.format(self.id, self.datetime, self.value)
+        return '<Record id={} city={} datetime={} value={}>'.format(self.id, self.city, self.datetime, self.value)
 
 db.init_app(app)
 db.create_all(app=app)
@@ -23,23 +24,23 @@ db.create_all(app=app)
 def particular_matter_samples(city):
     status, response = openaq_api.measurements(city=city, parameter='pm25')
     air_samples = response['results']
-    return [(sample['date']['utc'], sample['value']) for sample in air_samples]
+    return [(sample['date']['utc'], sample['city'], sample['value']) for sample in air_samples]
 
 @app.route('/')
 def index():
-    return str(particular_matter_samples('Los Angeles')) 
+    samples = Record.query.filter(Record.value >= 10).order_by(Record.value.desc()).all()
+    return render_template('samples.html', samples=samples) 
 
 @app.route('/refresh')
 def refresh():
     db.drop_all()
     db.create_all()
 
+    city = ' '.join([word.capitalize() for word in city.split('-')])
     samples = particular_matter_samples('Los Angeles')
-    records = [Record(datetime=date, value=measurement) for date, measurement in samples]	
+    records = [Record(datetime=date, city=city, value=measurement) for date, city, measurement in samples]	
 
     db.session.add_all(records)
     db.session.commit()
 
-    count = db.session.execute('select count(*) from record').fetchall()[0][0]
-
-    return 'Refreshed {} samples!'.format(count)
+    return redirect(url_for('index'), 302) 
