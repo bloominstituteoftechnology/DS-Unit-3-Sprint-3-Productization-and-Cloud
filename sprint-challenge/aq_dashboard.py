cd88f58440ef504b6f7d790cd925f5f17721428d
tesseract314 <tesseract314@users.noreply.github.com>
2019-04-12 12:57:19 -0700

    sc commit

--- sprint-challenge/aq_dashboard.py
+++ sprint-challenge/aq_dashboard.py
@@ -6,11 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 APP = Flask(__name__)
 api = openaq.OpenAQ()
 
-FLASK_ENV='development'
-status, resp = api.measurements(city='Los Angeles', parameter='pm25')
-
 def get_results():
-
+	status, resp = api.measurements(city='Los Angeles', parameter='pm25')
 	utcs = []
 	for utc in range(0, len(resp['results'])):
 		u = resp['results'][utc]['date']['utc']
@@ -25,37 +22,38 @@ def get_results():
 
 	return list(result)
 
-APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 DB = SQLAlchemy(APP)
+APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
 class Record(DB.Model):
 	id = DB.Column(DB.Integer, primary_key=True)
 	datetime = DB.Column(DB.String(25))
 	value = DB.Column(DB.Float, nullable=False)
-	def __repr(self):
+	
+	def __repr__(self):
 		tup = (self.datetime, self.value)
-		return '<Record {}>'.format(tup)
+		return '<Date: {}, Value: {}>'.format(tup)
 
 @APP.route('/')
 def root():
 	"""Base view."""
-	lofp = str(get_results())
-	return lofp
-
-def get_data():
-	dts = [Record(datetime=x[0]) for x in get_results()]
-	vals = [Record(datetime=x[1]) for x in get_results()]
-	DB.session.add(dts)
-	DB.session.add(vals)
-
+	data = Record.query.filter(Record.value >= 10).all()
+	return data
 
 @APP.route('/refresh')
 def refresh():
 	"""Pull fresh data from Open AQ and replace existing data."""
 	DB.drop_all()
 	DB.create_all()
-	get_data()
-	DB.session.commit()
+	api = openaq.OpenAQ()
+	status, resp = api.measurements(city='Los Angeles', parameter='pm25')
+	ids = [x for x in range(len(resp['results']))]
+	dts = [x['date']['utc'] for x in resp['results']]
+	vals = [x['value']for x in resp['results']]
+	for ids, dts, vals in zip(ids, dts, vals):
+		record = Record(id=ids, datetime=dts, value=vals)
+		DB.session.add(record)
+		DB.session.commit()
 	return 'Data refreshed!'
 
 	
