698845b052abd801df3d62d65f95e7e1b8f87013
ssingh1187 <sshilpa1187@users.noreply.github.com>
2019-04-12 12:24:35 -0700

    No data

--- Sprint Challenge/aq_dashboard.py
+++ Sprint Challenge/aq_dashboard.py
@@ -12,10 +12,31 @@ api = openaq.OpenAQ()
 @APP.route('/')
 def root():
     """Base view."""
-    record = Record.query.all()
-    return None
+    risky_air = Record.query.filter(Record.value >= 10.0).all()
+    return str(risky_air)
 
-def utc_value_list():
+@APP.route('/refresh')
+def refresh():
+    """Pull fresh data from Open AQ and replace existing data."""
+    DB.drop_all()
+    DB.create_all()
+    # TODO Get data from OpenAQ, make Record objects with it, and add to db
+    for x in utc_value_list(city='Los Angeles', parameter='pm25'):
+        r = Record(datetime = x[0], value = x[1])
+    DB.session.add(r)
+    DB.session.commit()
+    return 'Data refreshed!'
+
+class Record(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    datetime = DB.Column(DB.String(25))
+    value = DB.Column(DB.Float, nullable=False)
+
+    def __repr__(self):
+        return '< Time {} --- Value {} >'.format(self.datetime, self.value)
+
+
+def utc_value_list(city='Los Angeles', parameter='pm25'):
     """list of (utc_datetime, value) tuples"""
     status, body = api.measurements(city='Los Angeles', parameter='pm25')
     
@@ -24,26 +45,9 @@ def utc_value_list():
     for i in body['results']:
         utc_time = i['date']['utc']
         value = i['value']
-        new_list = new_list.append((utc_time,value))
+        new_list.append((utc_time,value))
 
     return new_list
 
-class Record(DB.Model):
-    id = DB.Column(DB.Integer, primary_key=True)
-    datetime = DB.Column(DB.String(25))
-    value = DB.Column(DB.Float, nullable=False)
+   
 
-    def __repr__(self):
-        return '< Time {} -- Value {} >'.format(self.datetime, self.value) 
-
-@APP.route('/refresh')
-def refresh():
-    """Pull fresh data from Open AQ and replace existing data."""
-    DB.drop_all()
-    DB.create_all()
-    # TODO Get data from OpenAQ, make Record objects with it, and add to db
-    for x in utc_value_list():
-        record = Record(datetime = x[0], value = x[1])
-    DB.session.add(record)
-    DB.session.commit()
-    return 'Data refreshed!'
