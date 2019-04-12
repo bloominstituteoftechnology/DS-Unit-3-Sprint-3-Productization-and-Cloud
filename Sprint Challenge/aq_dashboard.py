89f787843d3968d161cad6459911335f623a9cb1
ssingh1187 <sshilpa1187@users.noreply.github.com>
2019-04-12 13:24:19 -0700

    Without stretch goals.

--- Sprint Challenge/aq_dashboard.py
+++ Sprint Challenge/aq_dashboard.py
@@ -9,11 +9,20 @@ DB = SQLAlchemy(APP)
 
 api = openaq.OpenAQ()
 
+class Record(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    datetime = DB.Column(DB.String(25))
+    value = DB.Column(DB.Float, nullable=False)
+
+    def __repr__(self):
+        return '< Time {} --- Value {} >'.format(self.datetime, self.value)
+
 @APP.route('/')
 def root():
     """Base view."""
     risky_air = Record.query.filter(Record.value >= 10.0).all()
     return str(risky_air)
+    # return "hello"
 
 @APP.route('/refresh')
 def refresh():
@@ -23,18 +32,9 @@ def refresh():
     # TODO Get data from OpenAQ, make Record objects with it, and add to db
     for x in utc_value_list(city='Los Angeles', parameter='pm25'):
         r = Record(datetime = x[0], value = x[1])
-    DB.session.add(r)
-    DB.session.commit()
+        DB.session.add(r)
+        DB.session.commit()
     return 'Data refreshed!'
-
-class Record(DB.Model):
-    id = DB.Column(DB.Integer, primary_key=True)
-    datetime = DB.Column(DB.String(25))
-    value = DB.Column(DB.Float, nullable=False)
-
-    def __repr__(self):
-        return '< Time {} --- Value {} >'.format(self.datetime, self.value)
-
 
 def utc_value_list(city='Los Angeles', parameter='pm25'):
     """list of (utc_datetime, value) tuples"""
@@ -43,11 +43,13 @@ def utc_value_list(city='Los Angeles', parameter='pm25'):
     new_list = []
 
     for i in body['results']:
+        i['date']['utc']
         utc_time = i['date']['utc']
         value = i['value']
-        new_list.append((utc_time,value))
+        new_list.append(tuple((utc_time,value)))
 
     return new_list
+    # return str(body['results'])
 
    
 
