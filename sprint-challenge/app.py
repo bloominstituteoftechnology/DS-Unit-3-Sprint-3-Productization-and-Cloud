5c0e6673c2319325f5f7eb1b9606dfbdfe7615f7
Cole Hudson <cole@colejhudson.com>
2019-04-12 08:46:42 -0700

    added part 2

--- sprint-challenge/app.py
+++ sprint-challenge/app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+import openaq
+import json
 
 app = Flask(__name__)
 
+api = openaq.OpenAQ()
+
 @app.route('/')
 def index():
-    return 'TODO - part 2 and beyond!'
+    status, response = api.measurements(city='Los Angeles', parameter='pm25')
+    air_samples = response['results']
+    dated_samples = [(sample['date']['utc'], sample['value']) for sample in air_samples]
+    return str(dated_samples) 
