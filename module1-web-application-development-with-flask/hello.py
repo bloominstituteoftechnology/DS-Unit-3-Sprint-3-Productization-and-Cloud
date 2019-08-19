ef7f8a82be1d3703c4062828358ac30488a90dc2
Marvin A Davila <marvind@Marvins-MBP.fios-router.home>
2019-08-19 18:47:41 -0400

    Updated app

--- module1-web-application-development-with-flask/hello.py
+++ module1-web-application-development-with-flask/hello.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>\n bye"
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
