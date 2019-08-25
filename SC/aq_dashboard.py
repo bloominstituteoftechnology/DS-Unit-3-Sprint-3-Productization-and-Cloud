1d38f6270b3aa7e9e5b73625ecdfb0c7a0cf9956
Elizabeth Ter Sahakyan <30808123+elizabethts@users.noreply.github.com>
2019-08-25 16:27:04 -0700

    update files

--- SC/aq_dashboard.py
+++ SC/aq_dashboard.py
@@ -13,6 +13,7 @@ APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 DB = SQLAlchemy(APP)
 
 def retrieve_data():
+    """gets data from OpenAQ api and returns as a string of tuples"""
     api = openaq.OpenAQ()
     test = api.measurements(city='Los Angeles', parameter='pm25')
     number, dict = [i for i in test]
@@ -23,48 +24,9 @@ def retrieve_data():
     tuples_list = list(zip(utc_list, value_list))
     return str(tuples_list)
 
-
-class Record(DB.Model):
-    id = DB.Column(DB.Integer, primary_key=True)
-    datetime = DB.Column(DB.String(25))
-    the_value = DB.Column(DB.Float, nullable=False)
-
-    def __repr__(self):
-        return 'TODO - write a nice representation of Records'
-
-
-@APP.route('/refresh')
-def refresh():
-    """Pull fresh data from Open AQ and replace existing data."""
-    DB.drop_all()
-    DB.create_all()
-    api_data = retrieve_data()
-    conn = sqlite3.connect('db.sqlite3')
-    c = conn.cursor()
-    c.execute("CREATE TABLE Record (id varchar(3));")
-    c.executemany('INSERT INTO Record (datetime,value) VALUES (?,?)', api_data)
-    conn.commit()
-    conn.close()
-    # TODO Get data from OpenAQ, make Record objects with it, and add to db
-
-    DB.session.commit()
-    return 'Data refreshed!'
-
-@APP.route('/')
-def root():
-    """Base view."""
-    data = retrieve_data()
-    return data
-
-
-if __name__ == '__main__':
-    APP.run(port = 5000, debug=True)
-
-
-
-
-# -----------------------------------------
-def retrieve_data_2():
+def retrieve_data2():
+    """gets data from OpenAQ api and returns as a list of tuples
+       used to insert into the database"""
     api = openaq.OpenAQ()
     test = api.measurements(city='Los Angeles', parameter='pm25')
     number, dict = [i for i in test]
@@ -73,23 +35,59 @@ def retrieve_data_2():
     utc_list = [i['utc'] for i in date_dict]
     value_list = [i['value'] for i in results_dict]
     tuples_list = list(zip(utc_list, value_list))
-    return str(utc_list), value_list
+    return tuples_list
 
-utc_list, value_list = retrieve_data_2()
-conn = sqlite3.connect('db.sqlite3')
-c = conn.cursor()
-# c.execute("CREATE TABLE Record4 (id varchar(3),datetime VARCHAR(50), value INT(10));")
-# for i,j in zip(utc_list,value_list):
-#     c.execute("INSERT INTO Record4 (datetime, value) VALUES ({}, {})".format(i,j))
-# for entry in api_data:
-#     c.executemany('INSERT INTO Record2 (datetime,value) VALUES ({},{})'.format(api_data[0],api_data[1]))
-# conn.commit()
-# conn.close()
-# for i in api_data:
 
-# print([x[0] for x in api_data])
-# print(api_data)
-# stuff, nones = zip(*api_data)
-# print(type(api_data))
-# print(utc_list)
+class Record(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    datetime = DB.Column(DB.String(25))
+    value = DB.Column(DB.Float, nullable=False)
 
+    # def __repr__(self, id=id, datetime=datetime, value=value):
+    #     # write a nice representation of Records'
+    #     self.id = id
+    #     self.datetime = str(datetime)
+    #     self.value = value
+    #     return '{}, {}, {}'.format(self.id, self.datetime, self.value)
+    #     # i tried returning in a nice format
+    #     # this didn't work
+
+
+@APP.route('/refresh')
+def refresh():
+    """Pull fresh data from Open AQ and replace existing data."""
+    DB.drop_all()
+    DB.create_all()
+    api_data = retrieve_data2()
+
+    # enter the data into the sqlite3 database
+    conn = sqlite3.connect('db.sqlite3')
+    c = conn.cursor()
+    c.executemany('INSERT INTO Record (datetime,value) VALUES (?,?)', api_data)
+    conn.commit()
+    conn.close()
+    DB.session.commit()
+
+    return 'Data refreshed!'
+
+@APP.route('/')
+def root():
+    """Base view."""
+    data = retrieve_data()
+
+    # Get Records greater than or equal to 10
+    def greater_than_10():
+        conn = sqlite3.connect('db.sqlite3')
+        c = conn.cursor()
+        query = 'SELECT * FROM Record WHERE value >=10;'
+        answer = c.execute(query).fetchall()
+        return str(answer)
+
+    filtered_records = greater_than_10()
+    string = """ The data is: \n {}
+                 \n\n
+                 The records with values greater than or equal to 10 are: \n {}""".format(data, filtered_records)
+    return string
+
+if __name__ == '__main__':
+    APP.run(port = 5000, debug=True)
