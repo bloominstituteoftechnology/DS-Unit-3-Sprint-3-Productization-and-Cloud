6ed77abb547c84771a280ea6d68dc13f849d7770
Jordan Ireland <jordan.b.ireland@gmail.com>
2019-09-19 15:35:50 -0700

    fordeployment

---
+++ application/twitoff/__init__.py
@@ -0,0 +1,4 @@
+"""Entry point for TwitOff"""
+from .app import create_app
+
+APP = create_app()
---
+++ application/twitoff/models.py
@@ -0,0 +1,26 @@
+"""SQLAlchemy models for TwitOff"""
+from flask_sqlalchemy import SQLAlchemy
+
+DB = SQLAlchemy()
+
+
+class User(DB.Model):
+    """Twitter users that we pull and analyze tweets for."""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    name = DB.Column(DB.String(15), nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
+
+
+class Tweet(DB.Model):
+    """Tweets"""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(500))
+    embedding = DB.Column(DB.PickleType, nullable=False)
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+
+    def __repr__(self):
+        return '<Tweet {}>'.format(self.text)
