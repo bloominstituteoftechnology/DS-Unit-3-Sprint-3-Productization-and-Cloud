5b352866a4fa57a2c2650390be71ed9a0b0af653
ElisabethShah <ElisabethShah@users.noreply.github.com>
2019-10-13 18:18:32 -0700

    Pull data from API.

--- Sprint Challenge/aq_dashboard.py
+++ Sprint Challenge/aq_dashboard.py
@@ -1,10 +1,18 @@
 """OpenAQ Air Quality Dashboard with Flask."""
 from flask import Flask
+import openaq
 
 APP = Flask(__name__)
+
+
+def get_measurements(city='Los Angeles', parameter='pm25'):
+    api = openaq.OpenAQ()
+    status, body = api.measurements(city=city, parameter=parameter)
+    return [(result['date']['utc'],
+            result['value']) for result in body['results']]
 
 
 @APP.route('/')
 def root():
     """Base view."""
-    return 'TODO - part 2 and beyond!'
+    return str(get_measurements())
