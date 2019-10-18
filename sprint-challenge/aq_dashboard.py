57f8658d9b3569a440aaa0715b716b099cac541d
xander-bennett <xander-bennett@users.noreply.github.com>
2019-10-18 12:47:11 -0700

    imported API object from part 2

--- sprint-challenge/aq_dashboard.py
+++ sprint-challenge/aq_dashboard.py
@@ -1,8 +1,12 @@
 """OpenAQ Air Quality Dashboard with Flask."""
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from openaq import OpenAQ
 
 APP = Flask(__name__)
-
+APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+DB = SQLAlchemy(APP)
+API = OpenAQ()
 
 @APP.route('/')
 def root():
