c57014b535854e5e738c325a91bd87830899180e
xander-bennett <xander-bennett@users.noreply.github.com>
2019-10-28 12:02:59 -0700

    updated dashboard

--- sprint-challenge/aq_dashboard.py
+++ sprint-challenge/aq_dashboard.py
@@ -1,14 +1,43 @@
 """OpenAQ Air Quality Dashboard with Flask."""
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from openaq import OpenAQ
+import openaq
 
 APP = Flask(__name__)
+
 APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
 DB = SQLAlchemy(APP)
-API = OpenAQ()
+API = openaq.OpenAQ()
 
 @APP.route('/')
 def root():
     """Base view."""
-    return 'TODO - part 2 and beyond!'
+    return str(Record.query.all()[:5])
+
+@APP.route('/refresh')
+def refresh():
+    """Pull fresh data from Open AQ and replace existing data."""
+    DB.drop_all()
+    DB.create_all()
+    # TODO Get data from OpenAQ, make Record objects with it, and add to db
+    DB.session.commit()
+    return 'Data refreshed!'
+
+@APP.route('/result')
+def results():
+    """pull fresh data from open aq"""
+    _, body = API.measurements(city="Los Angeles", parameter='pm25')
+    results = []
+    for result in body['results']:
+        results.append((result['date']['utc'], result['value']))
+    return results
+
+class Record(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    datetime = DB.Column(DB.String(25))
+    value = DB.Column(DB.Float, nullable=False)
+
+    def __repr__(self):
+        return 'TODO - write a nice representation of Records'
