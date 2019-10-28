9925cd6844fac79bbe6ceee16c48db2cf4b00dca
xander-bennett <xander-bennett@users.noreply.github.com>
2019-10-28 12:44:32 -0700

    updating dashboard

--- sprint-challenge/aq_dashboard.py
+++ sprint-challenge/aq_dashboard.py
@@ -1,43 +1,46 @@
-"""OpenAQ Air Quality Dashboard with Flask."""
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import openaq
+from openaq import OpenAQ
 
 APP = Flask(__name__)
-
 APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-APP.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
 DB = SQLAlchemy(APP)
-API = openaq.OpenAQ()
+API = OpenAQ()
+
 
 @APP.route('/')
 def root():
-    """Base view."""
-    return str(Record.query.all()[:5])
+    """Base view, returns list of "potentially risky" air qualities."""
+    #return str(get_results())  # Part 2, pre-model solution, *all* results
+    return str(Record.query.filter(Record.value >= 10).all())
+
 
 @APP.route('/refresh')
 def refresh():
-    """Pull fresh data from Open AQ and replace existing data."""
+    """Replace existing data with data from OpenAQ."""
     DB.drop_all()
     DB.create_all()
-    # TODO Get data from OpenAQ, make Record objects with it, and add to db
+    for result in get_results():
+       record = Record(datetime=result[0], value=result[1])
+       DB.session.add(record)
     DB.session.commit()
     return 'Data refreshed!'
 
-@APP.route('/result')
-def results():
-    """pull fresh data from open aq"""
-    _, body = API.measurements(city="Los Angeles", parameter='pm25')
+
+def get_results():
+    """Pull fresh data from Open AQ, return as list of datetime/value tuples."""
+    _, body = API.measurements(city='Los Angeles', parameter='pm25')
     results = []
     for result in body['results']:
         results.append((result['date']['utc'], result['value']))
     return results
 
+
 class Record(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
-    datetime = DB.Column(DB.String(25))
+    datetime = DB.Column(DB.String(25))  # TODO real datetime
     value = DB.Column(DB.Float, nullable=False)
 
     def __repr__(self):
-        return 'TODO - write a nice representation of Records'
+        return '< Time {} --- Value {} >'.format(self.datetime, self.value)
