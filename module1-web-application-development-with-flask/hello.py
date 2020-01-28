9933c0c1aa68e157911e3afd9c14450a0c9358a8
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-01-28 13:55:10 -0800

    test commit from pycharm

--- module1-web-application-development-with-flask/hello.py
+++ module1-web-application-development-with-flask/hello.py
@@ -1,11 +1,12 @@
 from flask import Flask
-
-app = Flask(__name__)
+# global vars are capitalized
+APP = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-	return "Hello World!"
+	# deployed model goes here
+	return "<h1>Hello World!</h1>"
 
 
 if __name__ == "__main__":
