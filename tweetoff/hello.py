c74bf9e978631990ef58641a98966a275d8fd31d
Lord-Kanzler <climberalex@gmail.com>
2020-04-20 18:33:53 -0400

    created tweetoff folder and populated with hello.py web_app

---
+++ tweetoff/hello.py
@@ -0,0 +1,16 @@
+# hello.py
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    x = 2 + 2
+    return f"Hello World! {x}"
+
+
+@app.route("/about")
+def about():
+    return "About me"
