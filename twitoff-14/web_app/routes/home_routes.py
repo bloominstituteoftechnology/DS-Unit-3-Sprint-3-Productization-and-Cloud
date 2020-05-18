d782221837cbeaf32e463fb38e05f4a9d3a90b6d
Poenomaly <djbenson741@gmail.com>
2020-05-18 17:53:39 -0600

    App Skeleton

---
+++ twitoff-14/web_app/routes/home_routes.py
@@ -0,0 +1,14 @@
+# web_app/routes/home_routes.py
+
+from flask import Blueprint
+
+home_routes = Blueprint("home_routes", __name__)
+
+@home_routes.route("/")
+def index():
+    x = 2 + 2
+    return f"Hello World! {x}"
+
+@home_routes.route("/about")
+def about():
+    return "About me"
