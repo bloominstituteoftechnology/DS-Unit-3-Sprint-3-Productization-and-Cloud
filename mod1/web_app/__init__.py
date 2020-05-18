ff213f843ff69cc23ab419edad3b20508fc35807
mtoce <mtoce@villanova.edu>
2020-05-18 19:31:36 -0400

    finished base assig

--- mod1/web_app/__init__.py
+++ mod1/web_app/__init__.py
@@ -2,13 +2,24 @@
 
 from flask import Flask
 
+from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.tweet_routes import tweet_routes
 
+DATABASE_URI = "sqlite:///web_app_99.db" # using relative filepath
+#DATABASE_URI = "sqlite:////Users/Username/Desktop/your-repo-name/web_app_99.db" # using absolute filepath on Mac (recommended)
+#DATABASE_URI = "sqlite:///C:\\Users\\Username\\Desktop\\your-repo-name\\web_app_99.db" # using absolute filepath on Windows (recommended) h/t: https://stackoverflow.com/a/19262231/670433
+
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(home_routes)
     app.register_blueprint(tweet_routes)
+
     return app
 
 if __name__ == "__main__":
--- mod1/web_app/routes/tweet_routes.py
+++ mod1/web_app/routes/tweet_routes.py
@@ -1,6 +1,8 @@
 # web_app/routes/tweet_routes.py
 
-from flask import Blueprint, jsonify, request, render_template #, flash, redirect
+from flask import Blueprint, jsonify, request, render_template, flash, redirect
+
+from web_app.models import db, Tweet
 
 tweet_routes = Blueprint("tweet_routes", __name__)
 
@@ -15,12 +17,11 @@ def list_tweets():
 
 @tweet_routes.route("/tweets")
 def list_tweets_for_humans():
-    tweets = [
-        {"id": 1, "title": "tweet 1"},
-        {"id": 2, "title": "tweet 2"},
-        {"id": 3, "title": "tweet 3"},
-    ]
-    return render_template("tweets.html", message="Here's some tweets", tweets=tweets)
+    
+    # SELECT * FROM tweets
+    tweet_records = Tweet.query.all()
+    print(tweet_records)
+    return render_template("tweets.html", message="Here's some tweets", tweets=tweet_records)
 
 @tweet_routes.route("/tweets/new")
 def new_tweet():
@@ -30,9 +31,9 @@ def new_tweet():
 def create_tweet():
     print("FORM DATA:", dict(request.form))
     # todo: store in database
-    return jsonify({
-        "message": "tweet CREATED OK (TODO)",
-        "tweet": dict(request.form)
-    })
-    #flash(f"tweet '{new_tweet.title}' created successfully!", "success")
-    #return redirect(f"/tweets")
+
+    # INSERT INTO tweets ...
+    new_tweet = Tweet(title=request.form["title"], author_id=request.form["author_name"])
+    db.session.add(new_tweet)
+    db.session.commit()
+    return redirect("/tweets")
