4e1fac0283a613bbdc3e37bd440b5d1dfa652328
Poenomaly <djbenson741@gmail.com>
2020-05-18 18:00:06 -0600

    Display books json and html

--- twitoff-14/web_app/__init__.py
+++ twitoff-14/web_app/__init__.py
@@ -3,12 +3,12 @@
 from flask import Flask
 
 from web_app.routes.home_routes import home_routes
-#from web_app.routes.book_routes import book_routes
+from web_app.routes.book_routes import book_routes
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(home_routes)
-    #app.register_blueprint(book_routes)
+    app.register_blueprint(book_routes)
     return app
 
 if __name__ == "__main__":
