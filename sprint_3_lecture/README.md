a42b7f1c43589a9807dad4bcf8d247cbec0d58cc
Poenomaly <djbenson741@gmail.com>
2020-05-19 13:14:28 -0600

    Add twitter api and user/tweet database

--- sprint_3_lecture/README.md
+++ sprint_3_lecture/README.md
@@ -4,8 +4,19 @@ TODO: clone the repo
 ## Usage
 ```sh
 # mac version:
-FLASK_APP=web_app flask run
+FLASK_APP=twitoff_app flask run
 # windows version:
-set FLASK_APP=web_app
+set FLASK_APP=twitoff_app
 flask run
+```
+
+# database creation
+```
+# Windows users can omit the "FLASK_APP=twitoff_app" part...
+
+FLASK_APP=twitoff_app flask db init #> generates app/migrations dir
+
+# run both when changing the schema:
+FLASK_APP=twitoff_app flask db migrate #> creates the db (with "alembic_version" table)
+FLASK_APP=twitoff_app flask db upgrade #> creates the specified tables
 ```
--- sprint_3_lecture/twitoff_app/models.py
+++ sprint_3_lecture/twitoff_app/models.py
@@ -12,6 +12,22 @@ class Book(db.Model):
     title = db.Column(db.String(128))
     author_id = db.Column(db.String(128))
 
+class User(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    screen_name = db.Column(db.String(128), nullable=False)
+    name = db.Column(db.String)
+    location = db.Column(db.String)
+    followers_count = db.Column(db.Integer)
+    #latest_tweet_id = db.Column(db.BigInteger)
+
+class Tweet(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    user_id = db.Column(db.BigInteger, db.ForeignKey("user.id"))
+    full_text = db.Column(db.String(500))
+    embedding = db.Column(db.PickleType)
+
+    user = db.relationship("User", backref=db.backref("tweets", lazy=True))
+
 def parse_records(database_records):
     """
     A helper method for converting a list of database record objects into a list of dictionaries, so they can be returned as JSON
