7b992f1e6e246e3ce1fa0ca34231ecda1fa151a0
RAV10K1 <ravindra.tennekone@gmail.com>
2020-06-03 21:11:31 -0700

    added flask script in python

---
+++ module1-web-application-development-with-flask/hello.py
@@ -0,0 +1,14 @@
+# Simple local flask instance
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    x = 2 + 2
+    return f"Hello World! {x}"
+
+@app.route("/about")
+def about():
+    return "About me"
