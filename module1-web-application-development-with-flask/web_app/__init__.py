aa99371c3ca0054c2f01af4170ef109bdff28468
RAV10K1 <ravindra.tennekone@gmail.com>
2020-06-07 16:51:55 -0700

    setup dev db

--- module1-web-application-development-with-flask/web_app/__init__.py
+++ module1-web-application-development-with-flask/web_app/__init__.py
@@ -1,12 +1,23 @@
 from flask import Flask
 
+from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
 
+DATABASE_URI = "sqlite:///books_repo.db" # using relative filepath
+#DATABASE_URI = "sqlite:////Users/Username/Desktop/your-repo-name/web_app_99.db" # using absolute filepath on Mac (recommended)
+#DATABASE_URI = "sqlite:///C:\\Users\\Username\\Desktop\\your-repo-name\\web_app_99.db" # using absolute filepath on Windows (recommended) h/t: https://stackoverflow.com/a/19262231/670433
+
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
+
     return app
 
 if __name__ == "__main__":
