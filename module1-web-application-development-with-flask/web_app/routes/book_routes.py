3b834097fdec6d87a0b25908a5889b7fcd22e33f
RAV10K1 <ravindra.tennekone@gmail.com>
2020-06-07 17:16:01 -0700

    completed

--- module1-web-application-development-with-flask/web_app/routes/book_routes.py
+++ module1-web-application-development-with-flask/web_app/routes/book_routes.py
@@ -1,24 +1,30 @@
 from flask import Blueprint, jsonify, request, render_template #, flash, redirect
-from web_app.models import db, Book
+from web_app.models import db, Book, parse_records
 
 book_routes = Blueprint("book_routes", __name__)
 
 @book_routes.route("/books.json")
 def list_books():
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
+    book_records = Book.query.all()
+    print(book_records)
+    books = parse_records(book_records)
+
+#    books = [
+#       {"id": 1, "title": "Book 1"},
+#        {"id": 2, "title": "Book 2"},
+#        {"id": 3, "title": "Book 3"},]
+    
     return jsonify(books)
 
 @book_routes.route("/books")
 def list_books_for_humans():
     books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
+#        {"id": 1, "title": "Book 1"},
+#        {"id": 2, "title": "Book 2"},
+#        {"id": 3, "title": "Book 3"},]
+    book_records = Book.query.all()
+    print(book_records)
+    books = parse_records(book_records)
     return render_template("books.html", message="Here's some books", books=books)
 
 @book_routes.route("/books/new")
