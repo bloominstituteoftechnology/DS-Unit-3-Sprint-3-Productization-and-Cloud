89aa794e0e15b37f4291c3d565587372cd7e6082
britneh <brhacket@gmail.com>
2020-06-15 14:56:20 -0700

    set up web app

---
+++ module1-web-application-development-with-flask/web_app/__init__.py
@@ -0,0 +1,16 @@
+# web_app/__init__.py
+
+from flask import Flask
+
+from web_app.routes.home_routes import home_routes
+from web_app.routes.book_routes import book_routes
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(home_routes)
+    app.register_blueprint(book_routes)
+    return app
+
+if __name__ == "__main__":
+    my_app = create_app()
+    my_app.run(debug=True)
